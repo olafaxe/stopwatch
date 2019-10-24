8eba15be30f2a5103890d8299c582cc54c9d6ff1
Ola Faxe <olafaxe@gmail.com>
2019-10-24 13:09:59 +0200

    Updates

---
+++ precache-manifest.9488412a63ed072866f41b03f7ad6be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a3bbc2e9f7cc7af790a5f748e96046",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "43ea2c15645eb3c5ef30",
+    "url": "/stopwatch/static/css/main.12c7dd4b.chunk.css"
+  },
+  {
+    "revision": "152a73242a2eb5e369c2",
+    "url": "/stopwatch/static/js/2.3c5c74c7.chunk.js"
+  },
+  {
+    "revision": "43ea2c15645eb3c5ef30",
+    "url": "/stopwatch/static/js/main.b57164aa.chunk.js"
+  },
+  {
+    "revision": "65621fb5e328c1249913",
+    "url": "/stopwatch/static/js/runtime-main.ae4833d7.js"
+  }
+]);
